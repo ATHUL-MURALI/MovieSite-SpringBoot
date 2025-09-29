fcfe5a392bb07d5350ab7042007940830f530011
ATHUL-MURALI <anithaathul916@gmail.com>
2025-09-29 19:29:56 +0530

    Add reviews feature and not found page
    Implemented reviews functionality with UI and API integration, including review submission and display. Added a NotFound component for unmatched routes, updated routing in App.js, and improved navigation from the Hero carousel. Also enabled CORS in ReviewController for API access.

---
+++ movie-gold-v1/src/components/notFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+        <p>
+            Page Do Not Exist
+        </p>
+    </div>
+  )
+}
+
+export default NotFound
--- movie-gold-v1/src/components/reviews/Reviews.js
+++ movie-gold-v1/src/components/reviews/Reviews.js
@@ -1,10 +1,83 @@
 import { useParams } from "react-router-dom"
-import { useEffect,useRef } from "react"
+import { useEffect, useRef } from "react"
 import api from "../../api/axiosConfig"
-const Reviews = () => {
-  return (
-    <div>Reviews</div>
-  )
+import { Container, Row, Col } from "react-bootstrap"
+import ReviewForm from "../reviewForm/ReviewForm"
+
+const Reviews = ({ getMovieData, movie, reviews, setReviews }) => {
+    const revText = useRef();
+    let params = useParams();
+    const movieId = params.movieId;
+
+    useEffect(() => {
+        getMovieData(movieId);
+    }, [movieId]);
+
+
+    const addReview = async (e) => {
+        e.preventDefault();
+        const rev = revText.current;
+
+        try {
+            const response = await api.post("api/v1/reviews", { reviewBody: rev.value, imdbId: movieId });
+
+            const updateReviews = [...reviews, { body: rev.value }];
+
+            rev.value = "";
+
+            setReviews(updateReviews);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    return (
+        <Container>
+            <Row>
+                <Col><h3>Reviews</h3></Col>
+            </Row>
+            <Row className="mt-2">
+                <Col>
+                    <img src={movie?.poster} alt="" />
+                </Col>
+                <Col>
+                    {
+                        <>
+                            <Row>
+                                <Col>
+                                    <ReviewForm handleSubmit={addReview} revText={revText} labelText="Write a review?" />
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <hr />
+                                </Col>
+                            </Row>
+                        </>
+                    }
+                    {
+                        reviews?.map((r) => {
+                            return (
+                                <>
+                                    <Row>
+                                        <Col>{r.body}</Col>
+                                    </Row>
+                                    <Row>
+                                        <Col><hr /></Col>
+                                    </Row>
+                                </>
+                            )
+                        })
+                    }
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <hr />
+                </Col>
+            </Row>
+        </Container>
+    )
 }
 
 export default Reviews
